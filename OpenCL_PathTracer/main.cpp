f43e3f76543c4fafa0269fb2c4e021eccbd47381
shin_mbpr_late2013 <shocker.0x15@gmail.com>
2014-05-08 03:40:31 +0900

    Initial Commit

---
+++ OpenCL_PathTracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCL_PathTracer
+//
+//  Created by 渡部 心 on 2014/05/08.
+//  Copyright (c) 2014年 渡部 心. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
